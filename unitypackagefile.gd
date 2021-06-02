4d770454dee53adcebb3c457925934d6a32db780
Lyuma <xn.lyuma@gmail.com>
2021-06-01 23:32:55 -0700

    Add support for large unitypackage files using the OS-provided tar command

--- unitypackagefile.gd
+++ unitypackagefile.gd
@@ -1,6 +1,26 @@
 @tool
 extends Resource
 
+class ExtractedTarFile:
+	var fn: String = ""
+
+	func _init(filename: String):
+		fn = filename
+
+	func get_data() -> PackedByteArray:
+		# ALERT! Subarray is **inclusive** start and end index
+		var f = File.new()
+		f.open(fn, File.READ)
+		return f.get_buffer(f.get_length())
+
+	func get_string() -> String:
+		return get_data().get_string_from_utf8()
+
+	func get_stringfile() -> Object:
+		var f = File.new()
+		f.open(fn, File.READ)
+		return f
+
 const tarfile: GDScript = preload("./tarfile.gd")
 
 class UnityPackageAsset extends Reference:
@@ -18,13 +38,88 @@ var paths: Array = [].duplicate()
 var path_to_pkgasset: Dictionary = {}.duplicate()
 var guid_to_pkgasset: Dictionary = {}.duplicate()
 
+func external_tar_with_filename(source_file: String):
+	var stdout = [].duplicate()
+	var tmpdir = "temp_unityimp"
+	var d: Directory = Directory.new()
+	d.open("res://" + tmpdir)
+	d.make_dir("TAR_TEMP")
+	var full_tmpdir: String = tmpdir + "/TAR_TEMP"
+	var tar_args: Array = [
+		"-C", full_tmpdir,
+		"-zxvf", source_file.replace("res://", "")]
+	#if str(OS.get_name()) == "Windows" or str(OS.get_name()) == "UWP":
+	print("Running tar with " + str(tar_args))
+	OS.execute("tar", tar_args, stdout, true)
+	if stdout[0].find("resolve failed") != -1:
+		print("Rerunning with --force-local")
+		tar_args.append("--force-local")
+		stdout = []
+		OS.execute("tar", tar_args, stdout, true)
+	# print("executed " + str(tar_args) + ": " + str(stdout))
+	var out_lines = stdout[0].split("\n")
+	var guids_to_remove = [].duplicate()
+	for line in out_lines:
+		var fnparts: Array = line.strip_edges().split("/")
+		# x ./1234abcd0000ffff/
+		# x ./1234abcd0000ffff/preview.png
+		# x ./1234abcd0000ffff/asset
+		# x ./1234abcd0000ffff/asset.meta
+		# x ./1234abcd0000ffff/pathname
+		if len(fnparts) < 2:
+			continue
+		var guid: String = fnparts[0]
+		var type_part: String = fnparts[1]
+		if len(fnparts[1]) == 32:
+			if len(fnparts) < 3:
+				continue
+			guid = fnparts[1]
+			type_part = fnparts[2]
+		if len(guid) != 32:
+			push_error("Invalid member of .unitypackage: " + str(fnparts))
+			continue
+		if not guid_to_pkgasset.has(guid):
+			print("Discovered Asset " + guid)
+			guid_to_pkgasset[guid] = UnityPackageAsset.new()
+		var pkgasset: UnityPackageAsset = guid_to_pkgasset[guid]
+		pkgasset.guid = guid
+		var this_filename: String = full_tmpdir + "/" + str(guid) + "/" + str(type_part)
+		var header = ExtractedTarFile.new(this_filename)
+		if fnparts[1] == 'pathname':
+			pkgasset.pathname = header.get_data().get_string_from_utf8()
+			var path = pkgasset.pathname
+			if path.find("../") != -1 or path.find("/") == -1 or path.find("\\") != -1:
+				#if path != "Assets":
+				push_error("Asset " + pkgasset.guid + ": Illegal path " + path)
+				guids_to_remove.append(pkgasset.guid)
+			else:
+				# print("Asset " + pkgasset.guid + ": " + path)
+				path_to_pkgasset[path] = pkgasset
+				paths.push_back(path)
+		if fnparts[1] == 'preview.png':
+			pkgasset.icon = ImageTexture.new()
+			var image = Image.new()
+			image.load_png_from_buffer(header.get_data())
+			image.resize(20, 20)
+			pkgasset.icon.create_from_image(image)
+		if fnparts[1] == 'asset.meta':
+			pkgasset.metadata_tar_header = header
+		if fnparts[1] == 'asset':
+			pkgasset.asset_tar_header = header
 
-func init_with_filename(source_file):
+	for guid in guids_to_remove:
+		guid_to_pkgasset.erase(guid)
+	paths.sort()
+	return self
+
+func init_with_filename(source_file: String):
 	var file = File.new()
 	if file.open(source_file, File.READ) != OK:
 		return null
 
 	var flen: int = file.get_length() # NOTE: 32-bit only
+	if flen > 300000000:
+		return external_tar_with_filename(source_file)
 	var decompress_buf: PackedByteArray = file.get_buffer(flen).decompress(2147483647, 3) # COMPRESSION_GZIP = 3
 	print("Decompressed " + str(flen) + " to " + str(len(decompress_buf)))
 	var buf: PackedByteArray = PackedByteArray()
